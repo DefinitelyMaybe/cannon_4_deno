9c9e8c127ae00dff20d6823d4c41f509e3feec99
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-23 17:26:26 +1200

    changing the removal to outside of the walk

--- main.ts
+++ main.ts
@@ -18,26 +18,32 @@ if (errors == null) {
 }
 
 // then go through the src directory.
-// remove all map files and change all urls within .js files to reference the other .js files
+// change all urls within .js files to reference the other .js files
+const toRemove = []
 for (const entry of walkSync("./src")) {
   if (entry.name.endsWith("js")) {
     let data = Deno.readTextFileSync(entry.path);
     data = data.replaceAll(/\.ts/g, ".js");
     data = data.replace(/^/, `/// <reference types="./${entry.name.split(".")[0]}.ts" />\n`)
-    data = data.replaceAll(/\/\/# sourceMappingURL=.+?map/g, "");
+    data = data.replace(/\/\/# sourceMappingURL=.+?map/g, "");
     Deno.writeTextFileSync(entry.path, data);
   } else if (entry.name.endsWith("map")) {
-    try {
-      Deno.removeSync(entry.path); 
-    } catch (error) {
-      console.log(entry.path);
-    }
+    toRemove.push(entry.path)
   } else if (entry.name.endsWith("ts")) {
     let data = Deno.readTextFileSync(entry.path);
     data = data.replace(/^/, `/// <reference lib="dom" />\n`);
     Deno.writeTextFileSync(entry.path, data);
   }
 }
+
+// remove map files
+toRemove.forEach(path => {
+  try {
+    Deno.removeSync(path); 
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // fmt afterwards
 Deno.run({
